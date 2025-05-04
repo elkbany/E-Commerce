d2e0f85d4f1a90e13ce7ae5efe7622ce6ef79fa6
2bany <melkbany00@yahoo.com>
2025-05-04 17:41:47 +0300

    Add DA Layer

---
+++ E-Commerce.DA/Implementations/Base/GenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Commerce.DA.Context;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace E_Commerce.DA.Implementations.Base
+{
+    public class GenericRepository<T> where T : class
+    {
+        private readonly DBContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(DBContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
