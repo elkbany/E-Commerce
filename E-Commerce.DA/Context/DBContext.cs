4bda8206d0e51703833cf9f427f92d8a55d6d789
abdelrahman ahmed <ma01146356348@gmail.com>
2025-05-05 17:37:38 +0300

    dbset

--- E-Commerce.DA/Context/DBContext.cs
+++ E-Commerce.DA/Context/DBContext.cs
@@ -6,21 +6,31 @@ using System.Text;
 using System.Threading.Tasks;
 using E_Commerce.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace E_Commerce.DA.Context
 {
     public class DBContext : DbContext
     {
-        public DbSet<Product> Products { get; set; }
+        
 
         public DBContext(DbContextOptions<DBContext> options) : base(options)
         {
+           
+            //options.UseSqlServer("Data Source=.;Initial Catalog=E-Commerce;Integrated Security=True;Trust Server Certificate=True;");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
         }
 
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
 
 
     }
