74c82e4d1e797470743c43080596f5411eabec7c
abdelrahman ahmed <ma01146356348@gmail.com>
2025-05-05 16:15:42 +0300

    models

--- E-Commerce.Domain/Models/Product.cs
+++ E-Commerce.Domain/Models/Product.cs
@@ -3,5 +3,22 @@
     public class Product
     {
 
+       
+            public int Id  { get; set; }  
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            public decimal Price { get; set; }
+
+            public int UnitsInStock { get; set; }
+
+            public int CategoryID { get; set; }  
+
+            //public string? ImagePath { get; set; }  
+
+           
+            
+        }
     }
-}
+
