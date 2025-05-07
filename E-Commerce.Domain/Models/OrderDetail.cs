b3c2aa83d80d8f9f111d1c0da55be313714bd5cd
Nadasae <nadasaeed566@gmail.com>
2025-05-07 14:38:01 +0300

    product interface

--- E-Commerce.Domain/Models/OrderDetail.cs
+++ E-Commerce.Domain/Models/OrderDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,15 @@ namespace E_Commerce.Domain.Models
     {
         public int Id  { get; set; }  
 
+        [ForeignKey("Order")]
         public int  OrderID { get; set; }  
+
+        public Order order { get; set; }
+        [ForeignKey("Product")]
 
         public int  ProductID { get; set; } 
 
+        public Product product { get; set; }
         public int Quantity { get; set; }
 
        
