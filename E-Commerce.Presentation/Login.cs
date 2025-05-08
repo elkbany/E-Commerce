63f26a95937155c2a1fb153ea6357c848f0e36ce
Ahmed Ali <ahmedalisaied23@gmail.com>
2025-05-09 01:36:03 +0300

    Adding Login and Signup WinForm

--- E-Commerce.Presentation/Login.cs
+++ E-Commerce.Presentation/Login.cs
@@ -6,5 +6,29 @@ namespace E_Commerce.Presentation
         {
             InitializeComponent();
         }
+
+        private void login_registerHere_Click(object sender, EventArgs e)
+        {
+            Signup sForm = new Signup();
+            sForm.Show();
+            this.Hide();
+        }
+
+        private void login_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void login_showPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (login_showPassword.Checked)
+            {
+                login_password.PasswordChar = '\0';
+            }
+            else
+            {
+                login_password.PasswordChar = '*';
+            }
+        }
     }
 }
